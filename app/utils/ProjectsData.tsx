66308fd880a1253ad2960bbf6e666d28460578c6
Pirroxx <101422592+Genthh@users.noreply.github.com>
2024-11-20 15:19:29 +0100

    new commit

--- app/utils/ProjectsData.tsx
+++ app/utils/ProjectsData.tsx
@@ -9,83 +9,92 @@ export interface Project {
   position: "start" | "center" | "end";
   images?: string[];
 }
+
+const isBrowser = typeof window !== "undefined";
+
 const generateUUIDs = (): Project[] => {
-  const storedProjects = localStorage.getItem("projects");
-  if (storedProjects) {
-    return JSON.parse(storedProjects) as Project[];
+  if (isBrowser) {
+    const storedProjects = localStorage.getItem("projects");
+    if (storedProjects) {
+      return JSON.parse(storedProjects) as Project[];
+    }
+
+    const newProjects: Project[] = [
+      {
+        image: "/img1.jpg",
+        title: "Newz - Magazine Site",
+        row: 1,
+        width: "75%",
+        height: "550px",
+        position: "start",
+        uuid: uuidv4(),
+        images: [
+          "https://uithemez.com/i/hubfolio_HTML/inner_pages/assets/imgs/services/img1.jpg",
+          "https://uithemez.com/i/hubfolio_HTML/inner_pages/assets/imgs/services/img2.jpg",
+          "https://uithemez.com/i/hubfolio_HTML/inner_pages/assets/imgs/portfolio/project/3.jpg",
+        ],
+      },
+      {
+        image: "/img-4.jpg",
+        title: "LW Rebrand",
+        row: 2,
+        width: "35%",
+        height: "350px",
+        position: "start",
+        uuid: uuidv4(),
+      },
+      {
+        image: "/img-2.jpg",
+        title: "Dallas Ecolodge",
+        row: 2,
+        width: "55%",
+        height: "450px",
+        position: "end",
+        uuid: uuidv4(),
+      },
+      {
+        image: "/img-5.jpg",
+        title: "LW Rebrand",
+        row: 3,
+        width: "40%",
+        height: "350px",
+        position: "center",
+        uuid: uuidv4(),
+      },
+      {
+        image: "/img-6.jpg",
+        title: "Newz - Magazine Site",
+        row: 4,
+        width: "70%",
+        height: "450px",
+        position: "end",
+        uuid: uuidv4(),
+      },
+      {
+        image: "/img-2.jpg",
+        title: "Dallas Ecolodge",
+        row: 5,
+        width: "55%",
+        height: "450px",
+        position: "end",
+        uuid: uuidv4(),
+      },
+      {
+        image: "/img-4.jpg",
+        title: "LW Rebrand",
+        row: 5,
+        width: "35%",
+        height: "350px",
+        position: "center",
+        uuid: uuidv4(),
+      },
+    ];
+    localStorage.setItem("projects", JSON.stringify(newProjects));
+    return newProjects;
   }
 
-  const newProjects: Project[] = [
-    {
-      image: "/img1.jpg",
-      title: "Newz - Magazine Site",
-      row: 1,
-      width: "75%",
-      height: "550px",
-      position: "start",
-      uuid: uuidv4(),
-      images: [
-        "https://uithemez.com/i/hubfolio_HTML/inner_pages/assets/imgs/services/img1.jpg",
-        "https://uithemez.com/i/hubfolio_HTML/inner_pages/assets/imgs/services/img2.jpg",
-        "https://uithemez.com/i/hubfolio_HTML/inner_pages/assets/imgs/portfolio/project/3.jpg",
-      ],
-    },
-    {
-      image: "/img-4.jpg",
-      title: "LW Rebrand",
-      row: 2,
-      width: "35%",
-      height: "350px",
-      position: "start",
-      uuid: uuidv4(),
-    },
-    {
-      image: "/img-2.jpg",
-      title: "Dallas Ecolodge",
-      row: 2,
-      width: "55%",
-      height: "450px",
-      position: "end",
-      uuid: uuidv4(),
-    },
-    {
-      image: "/img-5.jpg",
-      title: "LW Rebrand",
-      row: 3,
-      width: "40%",
-      height: "350px",
-      position: "center",
-      uuid: uuidv4(),
-    },
-    {
-      image: "/img-6.jpg",
-      title: "Newz - Magazine Site",
-      row: 4,
-      width: "70%",
-      height: "450px",
-      position: "end",
-      uuid: uuidv4(),
-    },
-    {
-      image: "/img-2.jpg",
-      title: "Dallas Ecolodge",
-      row: 5,
-      width: "55%",
-      height: "450px",
-      position: "end",
-      uuid: uuidv4(),
-    },
-    {
-      image: "/img-4.jpg",
-      title: "LW Rebrand",
-      row: 5,
-      width: "35%",
-      height: "350px",
-      position: "center",
-      uuid: uuidv4(),
-    },
-  ];
-  localStorage.setItem("projects", JSON.stringify(newProjects));
-  return newProjects;
+  // Default return for server-side
+  return [];
 };
+
 export const projects: Project[] = generateUUIDs();
